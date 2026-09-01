078ad5357d39f7ebf535aeebdf0313a59474ab3c
Yashika Garg <gargy0189@gmail.com>
2026-09-01 16:36:48 +0530

    Added DSA questions

---
+++ Longest_Consecutive_sequence.java
@@ -0,0 +1,28 @@
+package com.chitkara;
+
+import java.util.Arrays;
+
+public class Longest_Consecutive_sequence {
+
+	public static void main(String[] args) {
+		int []arr= {100,4,200,1,3,2};
+		Arrays.sort(arr);
+	 
+		int longest=1;
+		int count=1;
+		for(int i=1; i<arr.length; i++) {
+			if(arr[i]==arr[i-1]+1) {
+				count++;
+				
+			}
+			else if(arr[i]!=arr[i-1]) {
+				count=1;
+				
+			}
+			longest=Math.max(count, longest);
+		}
+		System.out.println(longest);
+
+	}
+
+}
---
+++ Missing_number.java
@@ -0,0 +1,17 @@
+package com.chitkara;
+
+public class Missing_number {
+
+	public static void main(String[] args) {
+		int arr[]= {3,0,1};
+		int n=arr.length;
+		int number = 0;
+		for(int i=0; i<n; i++) {
+			number=n^arr[i];
+			
+		}
+		System.out.print(number);
+
+	}
+
+}
